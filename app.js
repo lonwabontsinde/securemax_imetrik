cc030389b0e165a831eba222dbca17918f6e16ca
lonwabontsinde <30588377+lonwabontsinde@users.noreply.github.com>
2020-11-09 07:52:25 +0200

    headers
    headers

--- app.js
+++ app.js
@@ -12,7 +12,22 @@ let deviceId;
 let device;
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 var express = require("express");
-app.use(express.json());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+	express.json();
+});
 
 io.on("connection", socket => {
   // Log whenever a user connects
